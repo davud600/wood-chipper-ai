e3506c688e094985243ed29d3e2c29e7dc0a4c9b
davud600 <davudg2002@gmail.com>
2025-05-07 14:52:16 +0000

    ...

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 source .venv/bin/activate
 redis-server --daemonize yes
-gunicorn -w 1 -b 0.0.0.0:8000 server:app --timeout 600 --threads 1 --daemon --access-logfile output.log --error-logfile output.log
+gunicorn -w 1 -b 0.0.0.0:8001 server:app --timeout 600 --threads 1 --daemon --access-logfile output.log --error-logfile output.log
